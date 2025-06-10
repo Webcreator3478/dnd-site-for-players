83dd73e50b9ad8d9a42ec49b8bde53474a5fee35
Webcreator3478 <lakshaya.0045@outlook.com>
2025-06-10 11:48:39 +0200

    Add content folder directly for GitHub Pages

---
+++ content/Index.md
@@ -0,0 +1,5 @@
+<h1>Homepage:</h1>
+
+Welcome to D&D obsidian pub by DM Laksh.
+
+LOL
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "/dnd-site-for-players",
     // --- THIS IS THE CRUCIAL CHANGE: UPDATED ignorePatterns ---
     ignorePatterns: ["private", "templates", ".obsidian", "Other but DM only", "The DM Private"],
     // --------------------------------------------------------
